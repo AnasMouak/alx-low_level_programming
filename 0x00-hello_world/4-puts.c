80778dc7f606b64ee1dd8b64d6a27b85ea0579f7
AnasMouak <anasmouak1111@gmail.com>
2023-10-05 08:52:59 -0700

    Update4_Task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
 
   return (0);
 }
-  
-

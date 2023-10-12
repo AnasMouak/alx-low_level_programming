fc40b87a95ae6ccf2058a0cecd6798ca6b188689
AnasMouak <anasmouak1111@gmail.com>
2023-10-12 08:50:48 -0700

    Update_Task_9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,25 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 }
-
 printf("\n");
 return (0);
 }

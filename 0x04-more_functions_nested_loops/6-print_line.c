b717f7ac685d8f37899c9ff307cec1c5ed7d20e1
AnasMouak <anasmouak1111@gmail.com>
2023-10-12 05:28:28 -0700

    Task_6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ *
+ * @n: parameter
+ * Returns: returns nothing
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (n == 0 || n < 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}

8bddeb4eae3eccb32ac6ccaef609aaad881c3c8f
AnasMouak <anasmouak1111@gmail.com>
2023-10-16 13:07:34 -0700

    Task_11_advanced

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+int main(void) 
+{
+char validChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int numValidChars = sizeof(validChars) - 1;
+
+srand(time(NULL));
+
+printf("Generated Password: ");
+
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+int index = rand() % numValidChars;
+putchar(validChars[index]);
+}
+
+printf("\n");
+
+return (0);
+}

8747a2d255e850506f742fc46ccb564cff416733
AnasMouak <anasmouak1111@gmail.com>
2023-10-16 08:16:12 -0700

    Task_7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - half of a string.
+ * @str: string to be printed.
+ */
+
+void puts_half(char *str)
+{
+int n = 0;
+int i;
+
+while (str[n] != '\0')
+{
+n++;
+}
+for (i = (n - 1) / 2; i < n; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}

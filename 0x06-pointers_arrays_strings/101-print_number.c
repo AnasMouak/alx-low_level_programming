f8f1b88de614c88a102e7571b362d45eb5421ac4
AnasMouak <anasmouak1111@gmail.com>
2023-10-19 03:11:56 -0700

    Update2_Task_9_advanced

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,15 +11,17 @@
 
 void print_number(int n)
 {
+unsigned int x;
+x = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+x = -n;
 }
 
-if (n / 10)
+if (x / 10)
 {
-print_number(n / 10);
+print_number(x / 10);
 }
-_putchar((n % 10) + '0');
+_putchar((x % 10) + '0');
 }

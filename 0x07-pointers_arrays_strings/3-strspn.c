1d0ae2c1ca2899ab1bf77a8443073b0d4a235571
AnasMouak <anasmouak1111@gmail.com>
2023-10-23 07:33:06 -0700

    Task_3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include <main.h>
+
+
+/**
+ * _strspn - a function that gets the
+ *           length of a prexif substring
+ *
+ * @s: pointer to string input
+ * @accept: substring prefix to look for
+ *
+ * Return: the number of bytes in the initial segment
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+int j;
+int n = 0;
+int d;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+d = 0;
+for (j = 0; s[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+d = 1;
+break;
+}
+}
+if (!d)
+{
+return (n);
+}
+n++;
+}
+return (n);
+}

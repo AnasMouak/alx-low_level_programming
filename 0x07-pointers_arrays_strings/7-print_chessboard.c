3f4156fef8351aa4042ad2a622a2f32b941511d1
AnasMouak <anasmouak1111@gmail.com>
2023-10-23 11:59:07 -0700

    Update_Task_6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,3 @@
-#include <ctype.h>
 #include "main.h"
 
 /**
@@ -13,35 +12,7 @@ void print_chessboard(char (*a)[8])
 {
 int i;
 int j;
-char vip[] = "rkbqkbkr";
-char sold[] = "pppppppp";
 
-for (i = 0; i < 8; i++)
-{
-for (j = 0; j < 8; j++)
-{
-if (i >= 2 && i <= 5 && j >= 0 && j <= 7)
-{
-a[i][j] = ' ';
-}
-else if (i == 1)
-{
-a[i][j] = sold[j];
-}
-else if (i == 6)
-{
-a[i][j] = toupper(sold[j]);
-}
-else if (i == 7)
-{
-a[i][j] = toupper(vip[j]);
-}
-else
-{
-a[i][j] = vip[j];
-}
-}
-}
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)

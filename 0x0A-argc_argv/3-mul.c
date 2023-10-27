0b735d182f842b28f3269676c60b1d9ef233b139
AnasMouak <anasmouak1111@gmail.com>
2023-10-27 09:50:35 -0700

    Update2_Task_3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,18 @@
 
 int main(int argc, char *argv[])
 {
+int result;
 
-if (argc != 3)
+if (argc == 3)
+{
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
+}
+else
 {
 printf("Error\n");
 return (1);
 }
-int result = atoi(argv[1]) * atoi(argv[2]);
-
-printf("%d\n", result);
 
 return (0);
 }

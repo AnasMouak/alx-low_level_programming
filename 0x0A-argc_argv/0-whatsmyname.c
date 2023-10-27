9fd941e0175fbbcb27c1849cbd6e54638718ed84
AnasMouak <anasmouak1111@gmail.com>
2023-10-27 09:16:02 -0700

    Update_Task_0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,10 @@
  * Return: 0 - success.
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 (void)argc;
-printf("%s\n", argv[i]);
+printf("%s\n", argv[0]);
 
 return (0);
 }

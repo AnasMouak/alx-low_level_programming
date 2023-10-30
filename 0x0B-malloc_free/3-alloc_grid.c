8420214f70676c796be0d17fa0cec4e2323ce167
AnasMouak <anasmouak1111@gmail.com>
2023-10-30 15:25:33 -0700

    Update4_Task_3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,13 +34,16 @@ for (i = 0; i < height; i++)
 s[i] = malloc(width * sizeof(int **));
 if (s[i] == 0)
 {
-while (i--)
+for (j = 0; j < i; j++)
 {
-free(s[i]);
+free(s[j]);
 }
 free(s);
 return (0);
 }
+}
+for (i = 0; i < height; i++)
+{
 for (j = 0; j < width; j++)
 {
 s[i][j] = 0;

d82ad870a9723f7829bd119eaf878b1b31d4426e
AnasMouak <anasmouak1111@gmail.com>
2023-10-31 09:33:27 -0700

    Update_Task_6_advanced

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,26 +10,26 @@
 
 int wordcount(char *s)
 {
-int i;
-int countw = 0;
-int inw = 0;
+    int i;
+    int countw = 0;
+    int inw = 0;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == ' ')
-{
-inw = 0;
-}
-else
-{
-if (!inw)
-{
-inw = 1;
-countw++;
-}
-}
-}
-return (countw);
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == ' ')
+        {
+            inw = 0;
+        }
+        else
+        {
+            if (!inw)
+            {
+                inw = 1;
+                countw++;
+            }
+        }
+    }
+    return countw;
 }
 
 /**
@@ -42,55 +42,92 @@ return (countw);
 
 char **strtow(char *str)
 {
-int i, words, k = 0, c = 0, start, end;
-char **s, *tmp;
-int size = 0;
+    if (str == NULL || *str == '\0')
+    {
+        return NULL;
+    }
 
-if (str == 0 || *str == '\0')
-{
-return (0);
-}
-while (str[size] != '\0')
-{
-size++;
-}
-words = wordcount(str);
+    int words = wordcount(str);
 
-s = (char **) malloc ((words + 1) * sizeof(char));
+    if (words == 0)
+    {
+        return NULL;
+    }
 
-if (s == 0)
-{
-return (0);
-}
+    char **s = (char **)malloc((words + 1) * sizeof(char *));
+    
+    if (s == NULL)
+    {
+        return NULL;
+    }
 
-for (i = 0; i < size; i++)
-{
+    int i, k = 0, c = 0, start = 0;
+    
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            if (c)
+            {
+                char *tmp = (char *)malloc(c + 1);
+                
+                if (tmp == NULL)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        free(s[j]);
+                    }
+                    free(s);
+                    return NULL;
+                }
 
-if (str[i] == '\0' || str[i] == ' ')
-{
-if (c)
-{
-end = i;
-tmp = (char *) malloc(sizeof(char) * (c + 1));
-if (tmp == 0)
-{
-return (0);
-}
-while (start < end)
-{
-*tmp++ = str[start++];
-}
-*tmp = '\0';
-s[k] = tmp - c;
-k++;
-c = 0;
-}
-}
-else if (c++ == 0)
-{
-start = i;
-}
-}
-s[k] = '\0';
-return (s);
+                int end = i;
+                int char_index = 0;
+                
+                while (start < end)
+                {
+                    tmp[char_index++] = str[start++];
+                }
+
+                tmp[char_index] = '\0';
+                s[k] = tmp;
+                k++;
+                c = 0;
+            }
+        }
+        else if (c++ == 0)
+        {
+            start = i;
+        }
+    }
+    
+    if (c)
+    {
+        char *tmp = (char *)malloc(c + 1);
+        
+        if (tmp == NULL)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                free(s[j]);
+            }
+            free(s);
+            return NULL;
+        }
+        
+        int end = i;
+        int char_index = 0;
+        
+        while (start < end)
+        {
+            tmp[char_index++] = str[start++];
+        }
+
+        tmp[char_index] = '\0';
+        s[k] = tmp;
+        k++;
+    }
+
+    s[k] = NULL;
+    return s;
 }

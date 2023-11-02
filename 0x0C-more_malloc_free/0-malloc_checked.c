6327c246883d2d81e08a7e4a70b30bab618e5caf
AnasMouak <anasmouak1111@gmail.com>
2023-11-02 07:15:13 -0700

    Task_0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process termination if error
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+void *s = malloc(b);
+
+if (s == 0)
+{
+exit(98);
+}
+
+return (s);
+}

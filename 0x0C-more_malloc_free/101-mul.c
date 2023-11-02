7d0460a5e5947a9ad9e704391602e81e92633315
AnasMouak <anasmouak1111@gmail.com>
2023-11-02 15:01:09 -0700

    Update_Task_5_advanced

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include "main.h"
 
+void _puts(const char *str) {
+    while (*str != '\0') {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n'); // Add a newline character at the end
+}
 
-bool isPositiveInteger(const char *str)
-    {
+// Function to check if a string is a positive integer
+bool isPositiveInteger(const char *str) {
     if (str == NULL || *str == '\0') {
         return false;
     }
 
     while (*str) {
-        if (!isdigit(*str))
-	{
+        if (!isdigit(*str)) {
             return false;
         }
         str++;
@@ -21,21 +28,39 @@ bool isPositiveInteger(const char *str)
     return true;
 }
 
-unsigned long long multiplyPositiveNumbers(unsigned long long num1, unsigned long long num2)
-{
+// Function to multiply two positive numbers
+unsigned long long multiplyPositiveNumbers(unsigned long long num1, unsigned long long num2) {
     return num1 * num2;
 }
 
-int main(int argc, char *argv[])
-{
+// Function to print an unsigned long long number using putchar
+void printUnsignedLongLong(unsigned long long num) {
+    if (num == 0) {
+        _putchar('0');
+        return;
+    }
+
+    char buffer[20]; // Max length for a 64-bit integer
+    int index = 0;
+
+    while (num > 0) {
+        buffer[index++] = '0' + num % 10;
+        num /= 10;
+    }
+
+    for (int i = index - 1; i >= 0; i--) {
+        _putchar(buffer[i]);
+    }
+}
+
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Error\n");
+        _puts("Error");
         return 98;
     }
 
-    if (!isPositiveInteger(argv[1]) || !isPositiveInteger(argv[2]))
-    {
-        printf("Error\n");
+    if (!isPositiveInteger(argv[1]) || !isPositiveInteger(argv[2])) {
+        _puts("Error");
         return 98;
     }
 
@@ -44,7 +69,8 @@ int main(int argc, char *argv[])
 
     unsigned long long result = multiplyPositiveNumbers(num1, num2);
 
-    printf("%llu\n", result);
+    printUnsignedLongLong(result);
+    _putchar('\n');
 
     return 0;
 }

8e250ce211ce41d9ee0c6c3c7e41ad553806881a
AnasMouak <anasmouak1111@gmail.com>
2023-11-05 04:35:09 -0800

    Update2_Task_3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,20 +20,9 @@ char *owner;
 
 /**
  * dog_t -Typedef for struct dog
- *
- * @name: name of dog
- * @age: age of the dog
- * @owner: dog owner
- *
  */
 
-typedef struct dog
-{
-char *name;
-float age;
-char *owner;
-
-} dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

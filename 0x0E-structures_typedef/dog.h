8eb3efee95cd95c2562a9815b47e7d1ebb01a804
AnasMouak <anasmouak1111@gmail.com>
2023-11-05 02:52:34 -0800

    Task_0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+#endif

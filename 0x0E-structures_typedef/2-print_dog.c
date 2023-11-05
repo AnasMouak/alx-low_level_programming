33842748ca8e70ec9010ad10e95d9a0e2a21d800
AnasMouak <anasmouak1111@gmail.com>
2023-11-05 04:08:42 -0800

    Task_2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+if (d != 0)
+{
+if (d->name == 0)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+if (d->age == 0)
+{
+printf("Age: (nil)\n");
+}
+else
+{
+printf("Age: %f\n", d->age);
+}
+if (d->owner == 0)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
+}

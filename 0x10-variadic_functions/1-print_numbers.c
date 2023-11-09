bd0f89fdf078c5490f54b44676f73eb229114d57
AnasMouak <anasmouak1111@gmail.com>
2023-11-09 03:48:24 -0800

    Update2_Task_1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator && i != 0)
+		if (i != 0)
 		{
 			printf("%s", separator);
 		}

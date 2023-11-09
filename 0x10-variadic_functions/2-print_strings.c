c2a55a6cbc8b6a3c41ffa334fc81eadf73376b99
AnasMouak <anasmouak1111@gmail.com>
2023-11-09 04:02:47 -0800

    Update_Task_2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,11 +12,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list p;
 	unsigned int i;
 
-	if (separator == 0)
-	{
-		return;
-	}
-
 	va_start(p, n);
 
 	for (i = 0; i < n; i++)

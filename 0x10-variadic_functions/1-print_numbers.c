b26e99f1ab135b580ef40584d82fc982abca59e8
AnasMouak <anasmouak1111@gmail.com>
2023-11-09 02:42:53 -0800

    Task_1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints num
+ * @separator: pointer param
+ * @n: param
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int i;
+
+	if (separator == 0)
+	{
+		return;
+	}
+
+	va_start(p, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator && i != 0)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(p, unsigned int));
+	}
+	printf("\n");
+	va_end(p);
+}

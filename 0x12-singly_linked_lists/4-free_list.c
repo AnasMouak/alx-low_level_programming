f9b72f68349518530a7d95dbf9162970a5b0dd39
AnasMouak <anasmouak1111@gmail.com>
2023-11-16 08:28:30 -0800

    Task_4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ * @head: A pointer to the list_t list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *n;
+
+	while (head != 0)
+	{
+		n = head->next;
+		free(head->str);
+		free(head);
+		head = n;
+	}
+}

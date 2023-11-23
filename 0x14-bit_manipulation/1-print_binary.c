6f4809ae6b8e7ec4050f4429879a039e53df7178
AnasMouak <anasmouak1111@gmail.com>
2023-11-23 06:46:08 -0800

    Update3_Task_1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,29 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned int t;
-
-	if (n > 0)
+	if (n > 1)
 	{
-		i = 0;
-		t = n;
-
-		while (t > 0)
-		{
-			t >>= 1;
-			i++;
-		}
-
-		while (i > 0)
-		{
-			i--;
-			_putchar(((n >> i) & 1) + '0');
-		}
+		print_binary(n >> 1);
 	}
-	else
-	{
-		_putchar('0');
-	}
-
+	_putchar((n & 1) + '0');
 }

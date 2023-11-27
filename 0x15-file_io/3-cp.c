ebd10552a15e214678a0a58f42a07f50fb9cea5a
AnasMouak <anasmouak1111@gmail.com>
2023-11-27 13:30:08 -0800

    Update6_Task_3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,15 +35,15 @@ int main(int ac, char **av)
         dprintf(2, "Error: Can't write to %s\n", av[2]);
         exit(99);
     }
-    
+    b_re = 1024;
+    while (b_re == 1024)
+    {
     b_re = read(file_from, buff, 1024);
     if (b_re == -1)
     {
         dprintf(2, "Error: Can't read from file %s\n", av[1]);
 	exit(98);
     }
-    while ((b_re = read(file_from, buff, 1024)) > 0)
-    {
     b_wr = write(file_to, buff, b_re);
     if (b_wr == -1)
     {

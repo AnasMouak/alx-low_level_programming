2709e783b3b34f8ebb122cf2ca2d697b031f246d
AnasMouak <anasmouak1111@gmail.com>
2023-11-27 08:21:19 -0800

    Update3_Task_1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_CREAT);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IRUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IRUSR);
 
 	if (fd == -1)
 	{

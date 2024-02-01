1956944dbedbf70869fb01e65224e88327e8c295
AnasMouak <anasmouak1111@gmail.com>
2024-02-01 10:52:41 -0800

    Update_Task_4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,14 +13,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current_node;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
+
 	if (current_node != 0)
 	{
 		if (strcmp(current_node->key, key) == 0)
 		{
 			return (current_node->value);
-		} 
+		}
 	}
 	return (0);
 }

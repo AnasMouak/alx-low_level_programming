b92c0c3f2cb2ac809576befbcd9de5538983dd89
AnasMouak <anasmouak1111@gmail.com>
2024-02-01 10:44:42 -0800

    Task_6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: Pointer to the hash table to be deleted.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current, *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
